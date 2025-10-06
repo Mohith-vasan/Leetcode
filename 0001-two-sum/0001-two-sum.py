09a8e0da485655af50bef29f2bfcf38b691934e9
Mohith-vasan <154245711+Mohith-vasan@users.noreply.github.com>
2025-10-06 07:48:00 +0530

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,18 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        numToInd = {}
+        if len(nums)==0 or len(nums)==1:
+            return
+        arr=[]
         for i in range(len(nums)):
-            if target - nums[i] in numToInd:
-                return [numToInd[target - nums[i]], i]
-            numToInd[nums[i]] = i
-        return []
+            arr.append([nums[i],i])
+        arr.sort()
+        Left,Right=0,len(arr)-1
+        while Left<Right:
+            mid=arr[Left][0]+arr[Right][0]
+            if mid==target:
+                return [arr[Left][1],arr[Right][1]]
+            elif mid<target:
+                Left+=1
+            else:
+                Right-=1
+    
